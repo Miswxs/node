c3097b318f93146e1b191ae9c3f23127c874cd0e
wanhwi.son <wanhwi.son@blockchainos.org>
2018-03-21 14:45:54 +0900

    First Node project

---
+++ bin/common.js
@@ -0,0 +1,17 @@
+/*
+@date :2018.3.21
+@example : common.jswww
+ */
+const colors = require('colors');
+
+/* console.log => log( 'text' ); */
+const log = console.log.bind(console);
+module.exports = {
+    'log': function (str) {
+        log(colors.blue(str));
+    },
+    'test': function () {
+        console.log(this);
+        console.log('bbb');
+    }
+};
