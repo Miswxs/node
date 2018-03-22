fa6cbca43e6fdd00e85fbe5b600e1dbf4ff97266
wanhwi.son <wanhwi.son@blockchainos.org>
2018-03-22 14:27:53 +0900

    express + jade

--- app.js
+++ app.js
@@ -9,27 +9,55 @@ const router = express.Router()
 const fs = require('fs')
 const hostname = '127.0.0.1'
 const PORT = process.env.PORT || 3000
-
 /*============================================
-*
-* router
-*
+* config
 ============================================*/
-
-function routerProvider(path) {
-    if (path === "/") {
-        res.send('<h1>TEST</h1>')
-    } else if (path === "/about/") {
-        res.send('<h1>About</h1>')
-    }
-}
-
+app.locals.pretty = true;
+app.set('views', './views')
+app.set('view engine', 'jade')
+app.use(express.static('public'))
 /*============================================
 *
 * router
 *
 ============================================*/
 let indexFile = __dirname + '/public/index.html'
+app.get('/template', (req, res) => {
+
+    res.render('temp',{time:Date(),title:'This is my title!!'})
+})
+app.get('/', (req, res) => {
+    res.send('test ')
+})
+app.get('/login', (req, res) => {
+    res.send('login')
+})
+app.get('/dynamic', (req, res) => {
+    var lis = '';
+    for (var i = 0; i < 5; i++) {
+        lis += '<li>coding</li>'
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link type="text/css" rel="StyleSheet" href="./css/common.css">
+</head>
+<body>
+<ul>
+${lis}
+</ul>
+${time}
+dynamic.html
+<p>test</p>
+</body>
+</html>`
+    res.send(output)
+})
+/*
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.sendFile(indexFile);
@@ -40,6 +68,8 @@ app.get('/about/', function (req, res) {
 router.get('/module/', (req, res) => {
     res.send('<h1>module</h1>')
 })
+*/
 app.listen(PORT, () => {
+    console.log('start SERVER')
     console.log(__dirname);
 })
